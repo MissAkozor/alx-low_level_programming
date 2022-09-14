01c11b3fc2258afa62b96b00cf98ba4fb0327421
Joanne Akozor <joanneakozor@gmail.com>
2022-09-14 08:14:47 -0700

    Alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet -prints alphabets in small letters
+ *
+ * description: prints alphabets with _putchar
+ *
+ * Return: void (success)
+ */
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
